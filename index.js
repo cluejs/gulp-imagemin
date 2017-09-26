5a4f5a62b0d8b2f61b45318a676076e15bf5fa97
Dmitry Schekotov <dark.knight.9111@gmail.com>
2017-09-26 19:59:58 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const gulp_imagemin = require('gulp-imagemin');
+const imagemin_mozjpeg = require('imagemin-mozjpeg');
+const imagemin_pngquant = require('imagemin-pngquant');
+const imagemin_svgo = require('imagemin-svgo');
+
+module.exports = () => {
+    gulp_imagemin([
+            imagemin_mozjpeg({
+                quality: 90,
+                progressive: true,
+            }),
+            imagemin_pngquant({}),
+            imagemin_svgo({
+                plugins: [
+                    {removeViewBox: false}
+                ]
+            })
+        ], {
+            verbose: true
+        }
+    )
+};
